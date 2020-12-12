4682ed217bb4e1f3ae5a63ad194f261bcf8b7bbe
vgfang <vincent_fang@sfu.ca>
2020-12-11 23:35:45 -0800

    remove directory

--- image-proc/test.py
+++
@@ -1,9 +0,0 @@
-import ctypes as ct # foreign function library
-
-imgproc = ct.cdll.LoadLibrary('./imgproc.so') # golang functions via shared libary
-class GoString(ct.Structure):
-    _fields_ = [("p", ct.c_char_p), ("n", ct.c_longlong)]
-
-
-imgproc.generate_blank.argtypes = [GoString, ct.c_longlong, ct.c_longlong]
-imgproc.generate_blank(GoString("apple.png".encode(),len(b"apple.png")),300,300)
